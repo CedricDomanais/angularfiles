e37eae07e18489116fda09736af23ef61a4dc65d
cedricdomanais <zeddomanais0421@gmail.com>
2024-03-27 19:56:01 +0800

    Your commit message

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const bodyParser = require ('body-parser');
 const cors = require('cors');
-
-
+const mongoose = require ('mongoose');
+const Post = require ('./models/post');
 const app = express();
 
 app.use(cors()); // Use the cors middleware
 
-
+mongoose.connect("mongodb+srv://zeddomanais0421:cedric04@cluster0.bwnspso.mongodb.net/node-angular?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=>{
+    console.log('Connected to the datebase!')
+})
+.catch(()=>{
+    console.log('Connection failed!')
+})
+,
 app.use(bodyParser.json());
 app.use (bodyParser.urlencoded({extended: false}));
 
